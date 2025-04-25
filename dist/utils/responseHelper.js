358dce1adecc4bf3433c56a6e9f91e5ae8584732
zas512 <zainalis.914@gmail.com>
2025-04-25 17:28:15 +0500

    fixes

--- dist/utils/responseHelper.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sendResponse = (res, status, success, message, data) => {
-    res.status(status).json(Object.assign({ success, message }, (data && { data })));
-};
-exports.default = sendResponse;
