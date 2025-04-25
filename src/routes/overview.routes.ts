49c0bfd3004840d862e097a51ce876f4b0b385e3
zas512 <zainalis.914@gmail.com>
2025-04-25 23:26:58 +0500

    fixes more

--- src/routes/overview.routes.ts
+++ src/routes/overview.routes.ts
@@ -1,13 +1,11 @@
 import { Router } from "express";
-import  { getDashboardOverview } from "../controllers/Overview.controller";
+import { getDashboardOverview } from "../controllers/Overview.controller";
 import requireAuth from "../middlewares/requireAuth";
 
 const router = Router();
 
 // All routes require authentication
 router.use(requireAuth);
-
 router.get("/get-overview", getDashboardOverview);
-
 
 export default router;
